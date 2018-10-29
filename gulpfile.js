5b5a5a47a2ed69d140695f1ff4ecd70aa254773e
Alecto <fomenkoandrey@inbox.ru>
2018-10-30 00:00:42 +0200

    исправление багов, оптимизация gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -33,23 +33,20 @@ global.$.path = {
   }
 };
 
-const comb    = require('../../gulp/tasks/comb.js').comb,
-  combUpdate  = require('../../gulp/tasks/comb.js').update,
-  combDelete  = require('../../gulp/tasks/comb.js').delete,
-  scss        = require('../../gulp/tasks/scss.js'),
-  mincss      = require('../../gulp/tasks/mincss.js'),
-  uglifyes    = require('../../gulp/tasks/uglify.js').uglifyes,
-  sync        = require('../../gulp/tasks/sync.js').sync,
-  syncInit    = require('../../gulp/tasks/sync.js').init;
+const { comb, combUpdate, combDelete }   = require('../../gulp/tasks/comb.js'),
+  scss     = require('../../gulp/tasks/scss.js'),
+  mincss   = require('../../gulp/tasks/mincss.js'),
+  uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
+  { sync, syncInit } = require('../../gulp/tasks/sync.js');
 
 function watchFiles () {
-  syncInit;
+  syncInit();
   watch($.path.scss.files, series(comb, scss, combDelete, mincss));
   watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
   watch($.path.html.files, sync);
 }
 
-task('combScssOnly', combUpdate);
+task('combScss', combUpdate);
 task('uglifyEs6', series(uglifyes, sync));
 task('sass2minCss', series(comb, scss, combDelete, mincss));
 task('watch', watchFiles);
