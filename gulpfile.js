993db135afc99e91215d389325bcf4254f15f341
Andrey Fomenko <fomenkoandrey1978@gmail.com>
2019-07-26 10:11:17 +0300

    обновление препроцессорного стиля.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@
 require('../../gulp/gulp-init.js')({HTML: '.'});
 
 const comb = require('../../gulp/tasks/comb.js'),
-    scss     = require('../../gulp/tasks/scss.js').scss,
+    scssDC     = require('../../gulp/tasks/scss.js').scssDC,
     mincss   = require('../../gulp/tasks/mincss.js'),
     uglifyes = require('../../gulp/tasks/uglify.js').uglifyes,
     { sync, syncInit } = require('../../gulp/tasks/sync.js');
 
 function watchFiles () {
     syncInit();
-    watch($.path.scss.files, series(scss, mincss));
-    watch([$.path.js.files, '!' + $.path.js.filesMin], series(uglifyes, sync));
-    watch($.path.html.files, sync);
+    watch($.PATH.scss.files, series(scssDC));
+    watch([$.PATH.js.files, '!' + $.PATH.js.filesMin], series(uglifyes, sync));
+    watch($.PATH.html.files, sync);
 }
 
 task('combScss', comb);
 task('uglifyEs6', series(uglifyes, sync));
-task('sass', series(scss));
+task('sass', series(scssDC));
 task('watch', watchFiles);
